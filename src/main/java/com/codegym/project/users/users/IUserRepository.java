a2c3c9788df028c9bb90de06c3aad600c34e9905
x-Beo-x <86937244+x-Beo-x@users.noreply.github.com>
2021-11-30 15:05:07 +0700

    ready for pull dev

--- src/main/java/com/codegym/project/users/users/IUserRepository.java
+++ src/main/java/com/codegym/project/users/users/IUserRepository.java
@@ -1,5 +1,6 @@
 package com.codegym.project.users.users;
 
+import com.codegym.project.role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

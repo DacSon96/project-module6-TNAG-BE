64fb8eecf900f490a0c3460ebd13e90f5e92eb13
vanhopld98 <vanhopld98@gmail.com>
2021-11-30 11:29:02 +0700

    create repo ,service category

---
+++ src/main/java/com/codegym/project/category/CategoryService.java
@@ -0,0 +1,35 @@
+package com.codegym.project.category;
+
+import com.codegym.project.IGeneralService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryService implements IGeneralService<Category> {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Page<Category> findAll(Pageable pageable) {
+        return categoryRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void delete(Category category) {
+        categoryRepository.delete(category);
+    }
+}
